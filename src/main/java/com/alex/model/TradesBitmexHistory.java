c044596a8f03d78c906527e9cf7de78c1045814c
amelnychuk <alexander.melnychuk@kstt.ua>
2018-05-08 18:07:04 +0300

    added getter

--- src/main/java/com/alex/model/TradesBitmexHistory.java
+++ src/main/java/com/alex/model/TradesBitmexHistory.java
@@ -1,6 +1,7 @@
 package com.alex.model;
 
 import lombok.Data;
+import lombok.Getter;
 import org.json.JSONObject;
 
 import java.math.BigDecimal;
@@ -8,6 +9,8 @@ import java.time.LocalDateTime;
 
 @Data
 public class TradesBitmexHistory {
+
+    @Getter
     private JSONObject dataObject = new JSONObject();
 
     public void addData(String ticker, String side, double price, BigDecimal size, BigDecimal total, LocalDateTime time){
@@ -18,9 +21,5 @@ public class TradesBitmexHistory {
         this.dataObject.put("size", size);
         this.dataObject.put("total", total);
         this.dataObject.put("time", time);
-    }
-
-    public JSONObject getDataObject() {
-        return dataObject;
     }
 }
