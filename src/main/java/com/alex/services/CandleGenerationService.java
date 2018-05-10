c32fc7f554b8c9b490c3dce66c87ca7be7d23f58
amelnychuk <alexander.melnychuk@kstt.ua>
2018-05-10 18:42:49 +0300

    Added part of store function

--- src/main/java/com/alex/services/CandleGenerationService.java
+++ src/main/java/com/alex/services/CandleGenerationService.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.concurrent.ConcurrentSkipListMap;
@@ -21,8 +23,22 @@ public class CandleGenerationService {
 
     @Getter
     @Setter
-    private Map<String, Map<String, Map<String, Map<LocalDateTime, Candle>>>> charts = new ConcurrentSkipListMap<>();
+    private Map<String,Map<LocalDateTime, Candle>> charts = new ConcurrentSkipListMap<>();
 
+    private void store(String period, LocalDateTime key, String message) {
+
+        charts.putIfAbsent(period, new ConcurrentSkipListMap<>());
+        Map<LocalDateTime, Candle> periodChart = charts.get(period);
+
+        BigDecimal price = message.getPrice().setScale(8, RoundingMode.HALF_UP);
+        if (periodChart.containsKey(key)) {
+            periodChart.get(key).update(price, message.getVolume(), message.getBaseVolume());
+        } else {
+            periodChart.put(key, new Candle(price, message.getVolume(), message.getBaseVolume()));
+
+            LocalDateTime completedKey = key.minusMinutes(Long.valueOf(period));
+            Candle candle = periodChart.get(completedKey);
+        }
+    }
 
 }
-
