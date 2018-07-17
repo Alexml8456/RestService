0bd38183ed656c0dba0ae4b55485c95bc507c94f
linchalex@gmail.com <eximb_com2007>
2018-07-17 23:13:57 +0300

    test for StringBuilder

--- src/main/java/com/alex/Main.java
+++ src/main/java/com/alex/Main.java
@@ -1,38 +1,42 @@
 package com.alex;
 
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
-
-import java.io.StringWriter;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 public class Main {
+
     public static void main(String[] args) {
+        Map<String, Map<LocalDateTime, Double>> tciValues = new ConcurrentSkipListMap<>();
+        tciValues.put("5", new ConcurrentSkipListMap<>());
+        tciValues.get("5").put(getGMTTimeMillis(),1.58);
+        tciValues.put("30", new ConcurrentSkipListMap<>());
+        tciValues.get("30").put(getGMTTimeMillis().minusMinutes(5),8.52);
+
 
 
         StringBuilder builder = new StringBuilder();
         builder.append("------Wave Trend indicators------\n");
+        tciValues.forEach((period, value) ->{
+            Double tci = value.entrySet().iterator().next().getValue();
+            LocalDateTime key = value.keySet().iterator().next();
+            builder.append("Period = ");
+            builder.append(period);
+            builder.append(" minutes;");
+            builder.append(" Timestamp = ");
+            builder.append(key);
+            builder.append("; Value = ");
+            builder.append(tci);
+            builder.append("!\n");
+        });
         builder.append("---------------------------------");
 
-
-
         System.out.println(builder.toString());
-        System.out.println(getGMTTimeMillis());
-
-
-        VelocityEngine ve = new VelocityEngine();
-        ve.init();
-        Template t = ve.getTemplate( "src/main/resources/helloworld.vm" );
-        VelocityContext context = new VelocityContext();
-        context.put("name", "World");
-        StringWriter writer = new StringWriter();
-        t.merge( context, writer );
-        System.out.println(writer.toString());
     }
 
     public static LocalDateTime getGMTTimeMillis() {
-        return LocalDateTime.now(ZoneId.of("GMT-0"));
+        return LocalDateTime.now(ZoneId.of("GMT-0")).truncatedTo(ChronoUnit.MINUTES);
     }
 }
